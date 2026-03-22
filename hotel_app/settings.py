7a0f05ba4add08693acf5cb4d7e8ca99b26bed36
SopuruchiCode <utahemmanuel452@gmail.com>
2026-03-22 19:15:21 +0100

    debug tru

--- hotel_app/settings.py
+++ hotel_app/settings.py
@@ -28,14 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hotel-app-gkpk.onrender.com',"127.0.0.1"]
 
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ 
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
